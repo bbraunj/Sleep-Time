0ef4ea0453c6080ee0c3a682472db7fbd2901694
Josh Braun <bbraunj@Joshs-MacBook-Pro.local>
2012-12-28 09:18:00 -0600

    Initial Commit

---
+++ Sleep Time/SleepAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SleepAppDelegate.h
+//  Sleep Time
+//
+//  Created by Josh Braun on 12/28/12.
+//  Copyright (c) 2012 Josh Braun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SleepAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Sleep Time/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Sleep Time
+//
+//  Created by Josh Braun on 12/28/12.
+//  Copyright (c) 2012 Josh Braun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "SleepAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([SleepAppDelegate class]));
+    }
+}
